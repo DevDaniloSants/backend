301657840437ecd4b83ced2fc787744965427a10
Danilo Santos <dev.danilosantos@gmail.com>
2025-04-28 19:54:36 -0300

    chore: add initial setup

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-plugin-prettier/recommended'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+    {
+        ignores: ['node_modules', 'dist/**'],
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,ts}'],
+        languageOptions: { globals: globals.browser },
+    },
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+])
