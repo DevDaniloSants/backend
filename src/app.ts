fff7d1db32cfcc9280191d850bcf02e6bc5ee700
Danilo Santos <dev.danilosantos@gmail.com>
2025-04-29 21:46:48 -0300

    feat: add custom error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express'
+import express, { Express, Response, Request, NextFunction } from 'express'
 import routes from './routes/routes'
 import cors from 'cors'
 
@@ -9,6 +9,7 @@ class App {
         this.server = express()
         this.middlewares()
         this.routes()
+        this.errorHandler()
     }
 
     middlewares() {
@@ -18,6 +19,24 @@ class App {
 
     routes() {
         this.server.use('/api', routes)
+    }
+
+    errorHandler() {
+        this.server.use(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            (err: Error, req: Request, res: Response, next: NextFunction) => {
+                if (err instanceof Error) {
+                    res.status(400).json({
+                        message: err.message,
+                    })
+                }
+
+                res.status(500).json({
+                    message: 'Ocorreu um erro no servidor',
+                    error: 'Internal Server Error',
+                })
+            }
+        )
     }
 }
 
