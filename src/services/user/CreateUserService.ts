7c6b7f8cf751cdfdabdd1a305adeceff1ea39940
Danilo Santos <dev.danilosantos@gmail.com>
2025-05-05 06:34:57 -0300

    refactor: refactor CreateUserService to use dependency injection

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,23 +1,33 @@
-import { getUserRepository, createUserRepository } from 'src/repository/user'
+import { ICreateUserRepository, IGetUserRepository } from 'src/repository/user'
 import { iUserRequest } from '../../interfaces/user'
+import { User } from '@prisma/client'
+import { IPasswordHasher } from 'src/adapters'
 
-class CreateUserService {
+export interface ICreateUserService {
+    execute(data: iUserRequest): Promise<User>
+}
+
+export class CreateUserService implements ICreateUserService {
+    constructor(
+        private createUserRepository: ICreateUserRepository,
+        private getUserRepository: IGetUserRepository,
+        private passwordHasher: IPasswordHasher
+    ) {}
     async execute({ name, email, password }: iUserRequest) {
-        const userAlreadyExists = await getUserRepository.getUserByEmail(email)
+        const userAlreadyExists =
+            await this.getUserRepository.getUserByEmail(email)
         if (userAlreadyExists) {
             throw new Error('User already exists')
         }
 
-        const user = await createUserRepository.create({
+        const passwordHash = await this.passwordHasher.hash(password)
+
+        const user = await this.createUserRepository.create({
             name,
             email,
-            password,
+            password: passwordHash,
         })
 
         return user
     }
 }
-
-const createUserService = new CreateUserService()
-
-export { createUserService }
