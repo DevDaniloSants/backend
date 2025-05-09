b09e7ba8d2ec0a520a86c7b80562bd146b297528
Danilo Santos <dev.danilosantos@gmail.com>
2025-05-08 21:49:55 -0300

    feat: add authUserController

---
+++ src/controllers/auth/AuthUserController.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+import { AuthUserService } from 'src/services/auth/AuthUserService'
+
+export class AuthUserController {
+    constructor(private authUserService: AuthUserService) {}
+    async handle(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email, password } = req.body
+
+            const user = await this.authUserService.execute({
+                email,
+                password,
+            })
+
+            res.json({ message: 'User authenticated', user })
+        } catch (error) {
+            next(error)
+        }
+    }
+}
