7d19f83c37eb06351871a9434930227aa52809fb
Danilo Santos <dev.danilosantos@gmail.com>
2025-05-13 21:12:38 -0300

    feat: add getUserById method to GetUserRepository

--- src/repository/user/GetUserRepository.ts
+++ src/repository/user/GetUserRepository.ts
@@ -1,15 +1,40 @@
-import { User } from '@prisma/client'
 import { prisma } from 'prisma/prisma'
 
+export interface IGetUserDTO {
+    id: string
+    name: string
+    email: string
+}
+
 export interface IGetUserRepository {
-    getUserByEmail(email: string): Promise<User | null>
+    getUserByEmail(email: string): Promise<IGetUserDTO | null>
+    getUserById(id: string): Promise<IGetUserDTO | null>
 }
 
 export class GetUserRepository implements IGetUserRepository {
-    async getUserByEmail(email: string): Promise<User | null> {
+    async getUserByEmail(email: string): Promise<IGetUserDTO | null> {
         const user = await prisma.user.findFirst({
             where: {
                 email,
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            },
+        })
+
+        return user
+    }
+    async getUserById(id: string): Promise<IGetUserDTO | null> {
+        const user = await prisma.user.findFirst({
+            where: {
+                id,
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
             },
         })
 
